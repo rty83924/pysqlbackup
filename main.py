a35245c9e2278327e410b37cda09fcd4c77ee9d5
Eric <rty83924@gmail.com>
2022-07-21 16:29:21 +0800

    update(main.py)

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import time
 import asyncio
+import sys
+import os
 from database import backup, delete, restore_dump
 from get_config import config
 from limit_asyncio import gather_with_concurrency
+
 async def main():
-    info = config(local_config=True)
+    try:
+        if sys.argv[2] == 'local':
+            info = config(local_config=True)
+        else:
+            print('please enter # python main.py local')
+            exit(1)
+    except IndexError:
+        info = os.environ
+    threads = int(sys.argv[1])
     mysqlhost = info['mysqlhost']
     user = info['user']
     passwd = info['passwd']
@@ -22,12 +33,9 @@ async def main():
         backup_task.append(backup(mysqlhost=mysqlhost, user=user, passwd=passwd, port=port, databases=data, outputpath=outputpath))
         dump_task.append(restore_dump(restorehost=restorehost,restoreuser=restoreuser,restorepwd=restorepwd, restoreport=restoreport, databases=data, inputpath=outputpath))
     #await asyncio.gather(*backup_task, asyncio.sleep(0.1))
-    await gather_with_concurrency(3, *backup_task, asyncio.sleep(0.1))
-    await gather_with_concurrency(3, *dump_task, asyncio.sleep(0.1))
+    await gather_with_concurrency(threads, *backup_task, asyncio.sleep(0.1))
+    await gather_with_concurrency(threads, *dump_task, asyncio.sleep(0.1))
     end = time.time()
     print(end - start)
 if __name__ == '__main__':
     asyncio.run(main(),debug=True)
-    #backup()
-    #dump()
-    #delete()
