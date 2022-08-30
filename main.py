55fe285300217f2b1cec3ce13b2dd5eda86b011e
Eric <rty83924@gmail.com>
2022-08-30 09:47:01 +0800

    table bug

--- main.py
+++ main.py
@@ -63,16 +63,17 @@ async def cloud(args):
     start = time.time()
     backup_task = list()
     dump_task = list()
+    for k, v in table.items():
+        await backup(mysqlhost=restorehost, user=restoreuser, passwd=restorepwd, port=restoreport, databases=k, table=v, outputpath=outputpath) 
+    await asyncio.sleep(10)  
     for data in databases:
         backup_task.append(backup(mysqlhost=mysqlhost, user=user, passwd=passwd, port=port, databases=data, outputpath=outputpath))
         dump_task.append(restore(restorehost=restorehost,restoreuser=restoreuser,restorepwd=restorepwd, restoreport=restoreport, databases=data, inputpath=outputpath))
-    for k, v in table.items():
-        await backup(mysqlhost=restorehost, user=restoreuser, passwd=restorepwd, port=restoreport, databases=k, table=v, outputpath=outputpath)    
     #await asyncio.gather(*backup_task, asyncio.sleep(0.1))
     await gather_with_concurrency(threads, *backup_task, asyncio.sleep(0.1))
     await gather_with_concurrency(threads, *dump_task, asyncio.sleep(0.1))
     for k, v in table.items():
-        await restore(restorehost=restorehost,restoreuser=restoreuser,restorepwd=restorepwd, restoreport=restoreport, databases=k, inputpath=outputpath, sqlfile='%s.sql' % v, table=v)   
+        await restore(restorehost=restorehost,restoreuser=restoreuser,restorepwd=restorepwd, restoreport=restoreport, databases=k, inputpath=outputpath, sqlfile='%s.sql.gz' % v, table=v)   
     end = time.time()
     print(end - start)
 
